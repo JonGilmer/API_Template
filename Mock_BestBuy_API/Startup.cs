f87410b48d01c60fc59d1361a28874d3417461e7
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-03-21 12:56:57 -0400

    Completed version of Mock BestBuy API Project

--- Mock_BestBuy_API/Startup.cs
+++ Mock_BestBuy_API/Startup.cs
@@ -6,8 +6,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,6 +29,26 @@ namespace Mock_BestBuy_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mock Best Buy API", Version = "v1" });
+            });
+
+            services.AddScoped<IDbConnection>(s =>
+            {
+                IDbConnection conn = new MySqlConnection(Configuration.GetConnectionString("bestbuy"));
+                conn.Open();
+                return conn;
+            });
+
+            services.AddTransient<IProductRepo, ProductRepo>();
+
+            services.AddCors(options =>                         // CORS = Cross Open Resource Sharing   
+            {                                                   // Opens up the Api to not just limit the callers to a specific domain or individual URL
+                options.AddPolicy("AllowOrigin", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,11 +57,17 @@ namespace Mock_BestBuy_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("AllowedOrigin");    //Added this in order to open up CORS
 
             app.UseAuthorization();
 
